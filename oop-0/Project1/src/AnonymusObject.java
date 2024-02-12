35074b045ff03f9c5210805d97ca7c3f9b1d6b8e
Soyaib10 <soyaibzihad10@gmail.com>
2024-02-12 18:39:01 +0600

    Anonymus fucntion

--- oop-0/Project1/src/AnonymusObject.java
+++ oop-0/Project1/src/AnonymusObject.java
@@ -1,6 +1,14 @@
 public class AnonymusObject {
     public static void main(String[] args) {
+        // normal object declaration
+        B obj = new B();
+        obj.age = 55;
+        System.out.println(obj.age);
+        obj.f();
 
+        // annonymus object declaration. you can't make any changes to class B() with annonymus function.
+        System.out.println(new B().age);
+        new B().f();
     }
 }
 
