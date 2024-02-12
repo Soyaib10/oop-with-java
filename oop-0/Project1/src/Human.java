c61c098bd1b8afcbe31d6f449b08bd07568aaa43
Soyaib10 <soyaibzihad10@gmail.com>
2024-02-12 17:34:09 +0600

    more on this keyword

--- oop-0/Project1/src/Human.java
+++ oop-0/Project1/src/Human.java
@@ -5,11 +5,21 @@ public class Human {
     double hight;
 
     // constructors
-    Human() {
-        // default constructor
-    }
-
     Human(String name) {
         this.name = name;
+    }
+
+    Human(String name, int age, double hight) {
+        this(name);
+        this.age = age;
+        this.hight = hight;
+    }
+
+    // method
+    void print() {
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(hight);
+        System.out.println();
     }
 }
--- oop-0/Project1/src/Main.java
+++ oop-0/Project1/src/Main.java
@@ -3,7 +3,11 @@
 public class Main {
     public static void main(String[] args) {
         // create and object
-        Human zihad = new Human("zihad"); // zihad is a reference object, meaning creating copy of the Human() class for mome
-        System.out.println(zihad.name);
+        Human obj1 = new Human("Zihad"); // obj1 is a reference object, meaning creating copy of the Human() class for mome
+        Human obj2 = new Human("Rrid", 22, 1.78);
+
+
+        obj1.print();
+        obj2.print();
     }
 }
