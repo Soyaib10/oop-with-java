a87b53a9866f06c82f5e97ade5937c995a8acc42
Soyaib10 <soyaibzihad10@gmail.com>
2024-03-06 23:25:00 +0600

    files deleted

--- oop-0/Project1/src/Array.java
+++
@@ -1,7 +0,0 @@
-public class Array {
-    public static void main(String[] args) {
-        int[] a = new int[3];
-        a[0] = 3;
-        System.out.println(a[0]);
-    }
-}
--- oop-0/Project1/src/Static1.java
+++
@@ -1,7 +0,0 @@
-public class Static1 {
-    public static void main(String[] args) {
-
-    }
-}
-
-
