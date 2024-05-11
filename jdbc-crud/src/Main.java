695321a145f0137a639a44f4c13d3f1ad180b5e9
Soyaib10 <soyaibzihad10@gmail.com>
2024-05-12 00:15:50 +0600

    all CRUD opetations

--- jdbc-crud/src/Main.java
+++ jdbc-crud/src/Main.java
@@ -17,37 +17,37 @@ public class Main {
         try {
             Connection connection = DriverManager.getConnection(url, userName, password);
             Statement statement = connection.createStatement(); // sql query executor
-            // dispaly data
-//            String query = "SELECT * FROM students";
-//            ResultSet resultSet = statement.executeQuery(query); // holds the table
-//            while (resultSet.next()) {
-//                int id = resultSet.getInt("id");
-//                String name = resultSet.getString("name");
-//                int age = resultSet.getInt("age");
-//                double marks = resultSet.getDouble("marks");
-//                System.out.println("ID: " + id);
-//                System.out.println("Name: " + name);
-//                System.out.println("Age: " + age);
-//                System.out.println("Marks: " + marks);
-//            }
+            // display data
+            String query = "SELECT * FROM students";
+            ResultSet resultSet = statement.executeQuery(query); // holds the table
+            while (resultSet.next()) {
+                int id = resultSet.getInt("id");
+                String name = resultSet.getString("name");
+                int age = resultSet.getInt("age");
+                double marks = resultSet.getDouble("marks");
+                System.out.println("ID: " + id);
+                System.out.println("Name: " + name);
+                System.out.println("Age: " + age);
+                System.out.println("Marks: " + marks);
+            }
 
             // insert data
-//            String insert = String.format("INSERT INTO students(name, age, marks) VALUES('%s', %o, %f)", "Rishan", 23, 110.0);
-//            int rowsAffected = statement.executeUpdate(insert);
-//            if (rowsAffected > 0) {
-//                System.out.println("Data inserted successfully!");
-//            } else {
-//                System.out.println("Data not inserted!");
-//            }
+            String insert = String.format("INSERT INTO students(name, age, marks) VALUES('%s', %o, %f)", "Rishan", 23, 110.0);
+            int rowsAffected = statement.executeUpdate(insert);
+            if (rowsAffected > 0) {
+                System.out.println("Data inserted successfully!");
+            } else {
+                System.out.println("Data not inserted!");
+            }
 
             // update
-//            String update = String.format("UPDATE students SET marks = %f WHERE id = %d", 111.5, 2);
-//            int rowsAffected = statement.executeUpdate(update);
-//            if (rowsAffected > 0) {
-//                System.out.println("Data Updated successfully!");
-//            } else {
-//                System.out.println("Data not Updated!");
-//            }
+            String update = String.format("UPDATE students SET marks = %f WHERE id = %d", 111.5, 2);
+            int rowsAffected = statement.executeUpdate(update);
+            if (rowsAffected > 0) {
+                System.out.println("Data Updated successfully!");
+            } else {
+                System.out.println("Data not Updated!");
+            }
 
 
             // delete
