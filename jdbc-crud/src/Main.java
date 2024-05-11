d1835d1b343c40f16665e3d134b1096a663bed9c
Soyaib10 <soyaibzihad10@gmail.com>
2024-05-11 23:29:57 +0600

    database to java data interection

--- jdbc-crud/src/Main.java
+++ jdbc-crud/src/Main.java
@@ -1,7 +1,7 @@
 import java.sql.*;
 
 public class Main {
-    private static final String url = "jdbc:mysql://localhost:3306/?user=root";
+    private static final String url = "jdbc:mysql://localhost:3306/jdbc-crud";
     private static final String userName = "root";
     private static final String password = "";
 
@@ -17,6 +17,18 @@ public class Main {
         try {
             Connection connection = DriverManager.getConnection(url, userName, password);
             Statement statement = connection.createStatement(); // sql query executor
+            String query = "SELECT * FROM students";
+            ResultSet resultSet = statement.executeQuery(query); // holds the table
+            while (resultSet.next()) {
+                int id = resultSet.getInt("id");
+                String name = resultSet.getString("name");
+                int age = resultSet.getInt("age");
+                double marks = resultSet.getDouble("marks");
+                System.out.println("ID: " + id);
+                System.out.println("Name: " + name);
+                System.out.println("Age: " + age);
+                System.out.println("Marks: " + marks);
+            }
         } catch (SQLException e) {
             System.out.println(e.getMessage());
         }
