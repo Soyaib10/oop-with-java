5c2c39cd220bb6f7332364b8e3795d1c0aaa05aa
Soyaib10 <soyaibzihad10@gmail.com>
2024-05-12 13:34:01 +0600

    insert-preparedstatement

---
+++ java-crud-preparedstatement/src/Main.java
@@ -0,0 +1,38 @@
+import java.sql.*;
+
+public class Main {
+    private static final String url = "jdbc:mysql://localhost:3306/jdbc-crud";
+    private static final String userName = "root";
+    private static final String password = "";
+
+    public static void main(String[] args) {
+        // load drivers
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            System.out.println(e.getMessage());
+        }
+
+        // database connection
+        try {
+            Connection connection = DriverManager.getConnection(url, userName, password);
+
+            // insert
+            String insert = "INSERT INTO students(name, age, marks) VALUES(?, ?, ?)";
+            // Statement statement = connection.createStatement();
+            PreparedStatement preparedStatement = connection.prepareStatement(insert);
+            preparedStatement.setString(1, "Zihad");
+            preparedStatement.setInt(2, 22);
+            preparedStatement.setDouble(3, 100.0);
+
+            int rowsAffected = preparedStatement.executeUpdate();
+            if (rowsAffected > 0) {
+                System.out.println("Data inserted Successfully!");
+            } else {
+                System.out.println("Data not Inserted!");
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
